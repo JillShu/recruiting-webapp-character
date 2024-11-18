db8379a4bcbd25f99130b2fa8d1f045006219d90
JillShu <jillshu0122@gmail.com>
2024-11-18 17:46:36 -0500

    adding class requirements selection

--- src/Components/Class2.js
+++ src/Components/Class2.js
@@ -12,15 +12,30 @@ function meetsClassRequirements(characterAttributes, minimumRequirements) {
 }
 
 const Class2 = ({attributes}) => {
+    const[selectedClass, setSelectedClass] = useState(Object.keys(CLASS_LIST)[0]);
     return (
             <div>
                 <h2>Classes</h2>
                 {Object.keys(CLASS_LIST).map((name) => (
-                    <div key={name} style = {{color:meetsClassRequirements(attributes,CLASS_LIST[name])?"red":"" }}> {name} 
-                    {/* <span>meet? { meetsClassRequirements(attributes,CLASS_LIST[name])?'yes':'no' }</span> */}
-                    </div>
+                   <div key={name}> 
+                        <div style = {{color:meetsClassRequirements(attributes,CLASS_LIST[name])?"red":"" }}> {name} </div>
+                        <button onClick={() => setSelectedClass(name)}/>
+                   </div> 
                 ))}
+                <ClassRequirements className={selectedClass} requirements={CLASS_LIST[selectedClass]}></ClassRequirements>
             </div>
      );
 }
+
+const ClassRequirements = ({className, requirements}) => {
+    return (
+        <div>
+            <h3>requirements for: {className} </h3>
+            {ATTRIBUTE_LIST.map((attributeName) => (
+                <ul key={attributeName}>{attributeName}:{requirements[attributeName]}</ul>
+            ))}
+        </div>
+    )
+}
+
 export default Class2;
